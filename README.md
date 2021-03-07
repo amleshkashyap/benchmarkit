410b4aa1cf56fe4a788181ce8fb1503fca9ec772
amleshkashyap <kashyapamlesh@gmail.com>
2021-03-07 21:24:06 +0530

    parse from file, store to string and array of strings, regenerate script using stored string, instance_eval string, pending string sanitization for security

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
     1. https://stackoverflow.com/questions/29327522/a-ruby-script-to-run-other-ruby-scripts?noredirect=1&lq=1
     2. https://stackoverflow.com/questions/6012930/how-to-read-lines-of-a-file-in-ruby
     3. https://blog.appsignal.com/2018/02/27/benchmarking-ruby-code.html
+    4. https://github.com/JuanitoFatas/fast-ruby
+    5. https://stackoverflow.com/questions/24685037/convert-string-to-a-function-in-ruby-on-rails
+    6. https://stackoverflow.com/questions/8590098/how-to-check-for-file-existence
+    7. https://api.rubyonrails.org/classes/ActionView/Helpers/SanitizeHelper.html
+    8. https://stackoverflow.com/questions/37807137/rails-what-is-sanitize-in-rails
+    9. 
--- lib/scripts/run_scripts.rb
+++ lib/scripts/run_scripts.rb
@@ -1,27 +1,83 @@
 require 'benchmark'
-# puts Benchmark.measure {
-#  10000.times do
-#    load "moreclasses.rb";
-#  end
-# }
 
-puts Benchmark.measure {
-  10000.times do
-    z = Time.new(2020, 8, 4, 10, 10, 10, "UTC")
-  end
-}
+$script_in_string = ""
+$script_in_array = []
 
-puts Benchmark.measure {
-  10000.times do
-    cur = Time.now.utc
-    s = "#{cur.year},#{cur.month},#{cur.day},#{cur.hour},#{cur.min},#{cur.sec}"
-    list = s.split(",")
-    z = Time.new(list[0], list[1], list[2], list[3], list[4], list[5], "UTC")
-  end
-}
+def measure_given_script(filename)
+  return false if !File.file?(filename)
+  time = Benchmark.measure {
+    100.times do
+      load filename
+    end
+  }
+  puts "Measuring script: " + filename + ", results: ", time
+end
 
-puts Benchmark.measure {
-  10000.times do
-    z = Time.now.utc.to_i
+def measure_time_snippets
+  puts "Time.new", Benchmark.measure {
+    10000.times do
+      z = Time.new(2020, 8, 4, 10, 10, 10, "UTC")
+    end
+  }
+
+  puts "Time.new from string", Benchmark.measure {
+    10000.times do
+      cur = Time.now.utc
+      s = "#{cur.year},#{cur.month},#{cur.day},#{cur.hour},#{cur.min},#{cur.sec}"
+      list = s.split(",")
+      z = Time.new(list[0], list[1], list[2], list[3], list[4], list[5], "UTC")
+    end
+  }
+
+  puts "Time.now.utc.to_i", Benchmark.measure {
+    10000.times do
+      z = Time.now.utc.to_i
+    end
+  }
+end
+
+def measure_given_snippet(snippet)
+  time = Benchmark.measure {
+    10000.times do
+      z = instance_eval snippet
+    end
+  }
+  puts "Measuring snippet: " + snippet, time
+end
+
+def execute_given_snippet(snippet)
+  ex = instance_eval snippet
+  p ex
+end
+
+def sanitize_this_line(snippet)
+end
+
+def read_and_store_script(filename)
+  $script_in_string = ""
+  $script_in_array = []
+  File.readlines(filename).each do |line|
+    next if line.chomp.nil? or line.strip[0].nil?
+    next if line.split("#")[0].strip[0].nil?
+    $script_in_string += line.chomp + ";"
+    $script_in_array.push(line.chomp)
   end
-}
+  p $script_in_string
+  p $script_in_array
+#  execute_given_snippet($script_in_string)
+  write_snippet_to_file($script_in_array)
+end
+
+def write_snippet_to_file(snippet_array)
+  File.open("test.rb", "w+") do |f|
+    f.puts(snippet_array)
+  end
+end
+
+# measure_time_snippets
+# measure_given_script("moreclasses.rb")
+# measure_given_script("/home/amlesh/projects/explore-rubyonrails/benchmarkit/lib/scripts/moreclasses.rb")
+# measure_given_snippet("Time.now.utc.to_i")
+# measure_given_snippet("100.times do; Time.now.utc; end; 100.times do; Time.now.utc.to_i; end;")
+read_and_store_script("/home/amlesh/projects/explore-rubyonrails/benchmarkit/lib/scripts/moreclasses.rb")
+read_and_store_script("moreclasses.rb")
